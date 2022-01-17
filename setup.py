2f2d21d8933fd2915b8e614941ee4bce19741423
Christian Eichenberger <christian.eichenberger@iarai.ac.at>
2022-01-17 19:06:08 +0100

    setup.py for lightning

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setuptools.setup(
     author='Christian Eichenberger, Moritz Neun',
     description='',
     url="https://github.com/iarai/NeurIPS2021-traffic4cast",
-    packages=setuptools.find_packages(include=['.*']),
+    packages=setuptools.find_packages(),
     package_dir = {
-        f't4c.{s}': s for s in setuptools.find_packages(include=['.*'])
+        f't4c.{s}': s for s in setuptools.find_packages()
     },
     python_requires=">=3.8",
 )
