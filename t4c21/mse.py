a4501b2a2c30ecedbdcf8ffd7525655e64eda4c5
Christian Eichenberger <christian.eichenberger@iarai.ac.at>
2022-01-21 15:41:15 +0100

    Wiedemann mask torch way

---
+++ t4c21/mse.py
@@ -0,0 +1,23 @@
+import torch
+from torch import functional as F
+from torch import Tensor
+from torch.nn import MSELoss
+
+VOL_CHANNELS = [0, 2, 4, 6]
+SPEED_CHANNELS = [1, 3, 5, 7]
+
+
+def mse_loss_wiedemann(input: Tensor, target: Tensor, reduction: str = "mean",) -> Tensor:
+    f = torch.count_nonzero(target[..., VOL_CHANNELS] != 0) / (
+        torch.count_nonzero(target[..., VOL_CHANNELS] != 0) + torch.count_nonzero(target[..., VOL_CHANNELS] == 0)
+    )
+    input[..., SPEED_CHANNELS] = input[..., SPEED_CHANNELS] * target[..., VOL_CHANNELS] != 0
+    return F.mse_loss(input, target, reduction=reduction) * f
+
+
+class MSELossWiedemann(MSELoss):
+    def __init__(self, size_average=None, reduce=None, reduction: str = "mean") -> None:
+        super(MSELossWiedemann, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        return mse_loss_wiedemann(input, target, reduction=self.reduction)
