20a9469f8b8497ff76e0ea6fa5ff76c74c46513c
Christian Eichenberger <christianmarkus@oakmountain.ch>
2022-01-24 23:50:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     url="https://github.com/iarai/NeurIPS2021-traffic4cast",
     packages=setuptools.find_packages(include=["t4c21", "t4c21.*"]),
     python_requires=">=3.8",
-    install_requires=["overrides>=6.1.0", "h5py>=3.1.0", "pytorch-lightning>=1.4.2", "numpy>=1.19.5", "torch>=1.8.0", "psutil>=5.8.0"],
+    install_requires=["overrides>=6.1.0", "h5py>=3.1.0", "pytorch-lightning>=1.4.2", "numpy>=1.20.0", "torch>=1.8.0", "psutil>=5.8.0"],
 )
